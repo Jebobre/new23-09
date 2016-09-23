47ec8f974977b37151282ab27b1ee3f8db19e2e8
egorrozinskiy <egor.rozinskiy@mail.ru>
2016-09-23 13:40:45 +0300

    f

---
+++ f5.py
@@ -0,0 +1,16 @@
+__author__ = 'student'
+#!/usr/bin/python3
+
+from pyrob.api import *
+
+
+@task
+def task_5_2():
+    for i in range(9):
+        if wall_is_beneath():
+            move_right()
+
+
+
+if __name__ == '__main__':
+    run_tasks()
