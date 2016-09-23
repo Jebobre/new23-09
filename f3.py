ccfaa031cd70881624c836c0616c3b99a87cbe47
egorrozinskiy <egor.rozinskiy@mail.ru>
2016-09-23 13:23:31 +0300

    f

--- f3.py
+++ f3.py
@@ -1,1 +1,18 @@
 __author__ = 'student'
+#!/usr/bin/python3
+
+from pyrob.api import *
+
+
+@task
+def task_3_1():
+
+    for i in range(9):
+        if not wall_is_on_the_right():
+            move_right()
+        else:
+            break
+
+if __name__ == '__main__':
+    run_tasks()
+
